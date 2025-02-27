c5fc02e28e32ccdeeb2d3d558fddababa11818ba
Juan Genova <juang@live.com.ar>
2025-02-27 16:55:25 +0100

    Practica Cypress Beta

--- cypress/e2e/practica.cy.js
+++ cypress/e2e/practica.cy.js
@@ -9,5 +9,10 @@ describe("the-saucedemo-login", () => {
         cy.get('[data-test="password"]').type('secret_sauce');
         cy.get('[data-test="login-button"]').click();
         cy.get('.title').should('have.text', 'Products'); //Verifica que entraste
+        /* ==== Generated with Cypress Studio ==== */
+        cy.get('#react-burger-menu-btn').click();
+        cy.get('[data-test="logout-sidebar-link"]').click();
+        cy.get('[data-test="login-button"]').should('be.visible');
+        /* ==== End Cypress Studio ==== */
     })
 })
