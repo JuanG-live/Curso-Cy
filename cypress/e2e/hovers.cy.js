2789c511a1b03824950455dfb2dade2fe93da0fb
Juan Genova <juang@live.com.ar>
2025-03-03 19:27:18 +0100

    Hovers

---
+++ cypress/e2e/hovers.cy.js
@@ -0,0 +1,26 @@
+/// <reference types="cypress" />
+import 'cypress-real-events';
+
+
+describe('The Internet herokuapp', ()=>{
+    it('hovers visible', ()=>{
+        cy.visit('https://the-internet.herokuapp.com/hovers');
+
+        cy.get('.figure').each(($el, index)=>{
+            cy.wrap($el).find("img").realHover();
+            cy.wrap($el).find('.figcaption > h5')
+                .should("be.visible")
+                .and("contain",`name: user${index + 1}` );
+        })
+    })
+    it('hover not visible', () =>{
+        cy.visit('https://the-internet.herokuapp.com/hovers');
+
+        cy.get('.figure').each(($el, index) => {
+            cy.wrap($el).find("img").realHover();
+            cy.wrap($el).find("img").trigger("mouseout") // Saco el mouse de la imagen
+            cy.wrap($el).find('.figcaption > h5')
+              .should("not.be.visible");  // Verifico que el texto desaparezca               
+        })
+    })
+})
